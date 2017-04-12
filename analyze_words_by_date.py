a00d7178374761b805180d27c907bfd73565db9d
Dan Cromartie <cromartied@gmail.com>
2017-04-11 23:09:20 -0400

    hands

---
+++ analyze_words_by_date.py
@@ -0,0 +1,22 @@
+from datetime import datetime
+import re
+import sys
+
+first_date = None
+last_date = None
+total_words = 0
+for line in sys.stdin:
+    line = line.strip()
+    count, date = re.split("\s+", line)
+    last_date = date
+    count = int(count)
+    total_words += count
+    if not first_date:
+        first_date = date
+
+first_date_obj = datetime.strptime(first_date, "%Y-%m-%d")
+last_date_obj = datetime.strptime(last_date, "%Y-%m-%d")
+total_days = (last_date_obj - first_date_obj).days
+print("Cards per day: %s" % (total_words * 1.0 / total_days))
+print("Total days: %s" % total_days)
+print("Total words: %s" % total_words)
