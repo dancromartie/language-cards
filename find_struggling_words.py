3082325771ccf06d21dc01b04e895d5541fffc7b
bar <foo>
2017-05-29 04:46:00 +0000

    hands

--- find_struggling_words.py
+++ find_struggling_words.py
@@ -8,7 +8,7 @@ import util
 cards = util.from_cookie_jar(sys.argv[1])
 data = pd.read_csv("drill_log.txt", sep=",", names=["id", "epoch", "interval"])
 gb = data.groupby("id")["interval"].agg(["mean", "count"])
-ids_struggling = list(gb[(gb["count"] > 1) & (gb["mean"] < 5)].index)
+ids_struggling = list(gb[(gb["count"] > 3) & (gb["mean"] < 5)].index)
 base_ids_struggling = set([re.sub(r"._to_.", "", id_) for id_ in ids_struggling])
 cards_struggling = [card for card in cards if card["id"] in base_ids_struggling]
 for cs in cards_struggling:
